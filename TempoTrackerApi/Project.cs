da8a15a0eda1fbee96f9e0f784c0249128b7347a
Beau Gunderson <beau@beaugunderson.com>
2011-04-07 13:34:06 -0700

    Adding TempoTrackerApi project.

---
+++ TempoTrackerApi/Project.cs
@@ -0,0 +1,19 @@
+namespace TempoTrackerApi
+{
+    public class Project
+    {
+        public Project(int id, string name)
+        {
+            Id = id;
+            Name = name;
+        }
+
+        public int Id { get; set; }
+        public string Name { get; set; }
+
+        public override string ToString()
+        {
+            return Name;
+        }
+    }
+}
